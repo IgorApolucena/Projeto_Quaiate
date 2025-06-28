7677434f088ff9b7ecdfbbc16d26862b45db6d29
Arthur Apolucena <apolucenaarthursantos>
2025-06-27 23:26:54 -0300

    lading page

--- php/login.php
+++ php/login.php
@@ -17,7 +17,7 @@ if ($resultado->num_rows > 0 and $tipo == 2) {
     $_SESSION['nome_session'] = $nome;
 
     // header('Location: dashboard.php');
-    header("Refresh: 2; url=dashboard.php");
+    header("Refresh: 2; url=../html/dashboard.html");
 }else{
     echo"Ops! Algo deu errado, tente novamente!";
     header("Refresh: 2; url=../html/login.html");
